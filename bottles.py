e1ec896d0d59e29a30bd9c887b8351217724daed
Will Carpenter <65402309+WillC3@users.noreply.github.com>
2025-06-23 09:22:25 +0100

    Format bottles.py

--- bottles.py
+++ bottles.py
@@ -9,40 +9,31 @@ class Bottles:
 
     @classmethod
     def verse(self, number: int):
-        match number:
-            case 0:
-                return (
-                    "No more bottles of beer on the wall, "
-                    "no more bottles of beer.\n"
-                    "Go to the store and buy some more, "
-                    "99 bottles of beer on the wall.\n"
-                )
-            case 1:
-                return (
-                    "1 bottle of beer on the wall, "
-                    "1 bottle of beer.\n"
-                    "Take it down and pass it around, "
-                    "no more bottles of beer on the wall.\n"
-                )
-            case 2:
-                return (
-                    "2 bottles of beer on the wall, "
-                    "2 bottles of beer.\n"
-                    "Take one down and pass it around, "
-                    "1 bottle of beer on the wall.\n"
-                )
-            case _:
-                return (
-                    f"{number} bottles of beer on the wall, "
-                    f"{number} bottles of beer.\n"
-                    "Take one down and pass it around, "
-                    f"{number - 1} bottles of beer on the wall.\n"
-                )
-
-
-if __name__ == "__main__":
-    print("#1")
-    print(Bottles.song())
-
-    print("#2")
-    print(Bottles.verses(4, 1))
+        if number == 0:
+            return (
+                "No more bottles of beer on the wall, "
+                "no more bottles of beer.\n"
+                "Go to the store and buy some more, "
+                "99 bottles of beer on the wall.\n"
+            )
+        elif number == 1:
+            return (
+                "1 bottle of beer on the wall, "
+                "1 bottle of beer.\n"
+                "Take it down and pass it around, "
+                "no more bottles of beer on the wall.\n"
+            )
+        elif number == 2:
+            return (
+                "2 bottles of beer on the wall, "
+                "2 bottles of beer.\n"
+                "Take one down and pass it around, "
+                "1 bottle of beer on the wall.\n"
+            )
+        else:
+            return (
+                f"{number} bottles of beer on the wall, "
+                f"{number} bottles of beer.\n"
+                "Take one down and pass it around, "
+                f"{number - 1} bottles of beer on the wall.\n"
+            )
